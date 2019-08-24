02cbbbdc9756ad2ee35d459f8f1190296f39f4d0
Agustín Benassi <agusbenassi@gmail.com>
2019-08-24 17:17:25 -0300

    Cambio versión.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='proyecto-next-journal',
-    version='0.1.0',
+    version='0.1.1',
     description="Descripción corta del proyecto.",
     author="Magdalena Segura",
     url='https://github.com/maggiegsegura/proyecto-next-journal',
